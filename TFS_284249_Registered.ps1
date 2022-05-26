14ca429274898479119f28ac2b3c2517408b9deb
Deniskaponchik <deniskaponchik@gmail.com>
2022-05-26 09:02:12 +0800

    Update TFS_284249_Registered.ps1

--- TFS_284249_Registered.ps1
+++ TFS_284249_Registered.ps1
@@ -1,60 +1,263 @@
-﻿# Get-Command -Module SQLPS
+
+# Create folder for csv file
+[Environment]::NewLine
+#($myInvocation.MyCommand.Name).Basename 
+$NameScript = (Get-Item $myInvocation.myCommand.path).Basename
+$PathScript = (Get-Item $myInvocation.myCommand.path).DirectoryName
+$PathScript
+
+
+$CsvPath = Read-Host "Куда выложить csv (По умолчанию создастся папка в директории со скриптом)"
+if ($CsvPath -eq '') {
+    New-Item -Path . -Name $NameScript -ItemType "Directory"
+    $CsvPath = "$PathScript\$NameScript"
+}
+$CsvPath
+[Environment]::NewLine
+
+
+$FileSQL = Read-Host "Путь до sql-файла (по умолчанию директория со скриптом)"
+if ($FileSQL -eq '') {
+#[System.IO.Path]::GetFileNameWithoutExtension("$myInvocation.myCommand.path")
+#[System.IO.Path]::GetFileNameWithoutExtension($myInvocation.MyCommand.Name)
+#[string]$FileSQL = [System.IO.Path]::Combine(([System.IO.Path]::GetDirectoryName($MyInvocation.InvocationName)), ([System.IO.Path]::ChangeExtension($MyInvocation.InvocationName, "sql" )))
+[string]$FileSQL = [System.IO.Path]::Combine(([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Name)), ([System.IO.Path]::ChangeExtension($MyInvocation.MyCommand.Name, "sql" )))
+}
+$FileSQL
+$FileSqlTxt = Get-Content $FileSQL -Encoding UTF8 -Raw
+$Query = @"
+$FileSqlTxt
+"@
+[Environment]::NewLine
+
+# Server, base, table, auth
+[Environment]::NewLine
+#$dataSource = “server\instance”
+ $dataSource = Read-Host "Адрес сервера (по умолчанию server\instance)"
+ if ($dataSource -eq '') {
+    $dataSource = "server\instance"
+ }
+ $dataSource
+ [Environment]::NewLine
+
+
+[Environment]::NewLine
+#$dataSource = “server\instance”
+#$dataSource = "ServerName"
+$database = Read-Host "База данных (по умолчанию database )"
+ if ($database -eq '') {
+    $database = "database"
+ }
+ $database
+ [Environment]::NewLine
+
+
+#$TableName = "dbo.account"
+
+ $auth = “Integrated Security=SSPI;”
+#$auth = Get-Credential
+
+
+[Environment]::NewLine
+[DateTime]$StartDateTime = "2022-01-01"
+ $StartDateTime = Read-Host "Start Date ( yyyy-MM-dd ) По умолчанию: 2022-01-01 "
+ $StartDateTime.DateTime
+
+ 
+ [Environment]::NewLine
+ [DateTime]$EndDateTime = Get-Date
+ $EndDateTime = Read-Host "  End Date ( yyyy-MM-dd ) По умолчанию: Get-Date "
+ $EndDateTime.DateTime
+ 
+
+
+
+
+###   SQLconnect   ###
+<# РАССКОММЕНТИРУЙ ЭТО, ЕСЛИ БУДЕШЬ ЗАПУСКАТЬ СКРИПТ НЕ ЦЕЛЬНЫМ ФАЙЛОМ, А ТОЛЬКО ЧАСТЬ. 
+$FileSqlTxt = Get-Content "D:\Scripts\PowerShell\SQL\TFS_284249_Closed_v5.0.sql" -Encoding UTF8 -Raw
+$CsvPath = "D:\Scripts\PowerShell\SQL\TFS_284249_Closed_v5.0"
+
+$Query = @"
+$FileSqlTxt
+"@
+$dataSource = "Server"       # 
+$database = "Database"
+$auth = "Integrated Security=SSPI;"
+[DateTime]$StartDateTime = "2022-01-01"       # 2022-01-01 2021-07-25  
+[DateTime]$EndDateTime = Get-Date        # Get-Date   "2021-07-27"
+#>
+
+# Итоговая строка подключения
+$connectionString = "Data Source=$DataSource; Initial Catalog=$database; $auth"
+#$connectionString = "Data Source=t2ru-tr-tst-02; Initial Catalog=BPMonline7_8888; Integrated Security=SSPI"
+
+while ($StartDateTime -le $EndDateTime) {
+
+    [Environment]::NewLine
+   #$StartDateTime.ToString("yyyy-MM-dd")     
+   #$StartDate = $StartDateTime.ToString("yyyy-MM-dd")  # не работает
+    $DateForSqlVariable = '{0:yyyy-MM-dd}' -f $StartDateTime
+    $DateForSqlVariable
+    $DateForCsvName = '{0:dd.MM.yyyy}' -f $StartDateTime
+    
+
+    $d1 = Get-Date  # Фиксируем время начала отработки скрипта
+
+    $con = new-object "System.data.sqlclient.SQLconnection"
+    $con.ConnectionString =$ConnectionString
+    $con.open()
+    $sqlcmd = new-object "System.data.sqlclient.sqlcommand"
+    $sqlcmd.connection = $con
+    $sqlcmd.CommandTimeout = 10000000
+    $sqlcmd.CommandText = $Query
+    $sqlCmd.Parameters.Add('@StartDate',$DateForSqlVariable)
+    $result = $sqlcmd.ExecuteReader()
+    $Table = [System.Data.DataTable]::new()
+    $Table.Load($result)
+    $con.Close()
+    $Table.Columns.ColumnName | % { $Table.Columns[$_].ColumnName = $_.ToUpper()}
+
+    $Table | Export-Csv -Path "$CsvPath\$DateForCsvName.csv" -Encoding UTF8 -NoTypeInformation
+    # | Export-Csv -Path "$PathScript\$NameScript\SR_BPM7_$startDateFormat.csv" -Encoding UTF8 -Delimiter "^" -NoTypeInformation
+
+    $d2 = Get-Date  # Фиксируем время окончания отработки скрипта
+    WRITE-Host 'Время выполнения:' -ForegroundColor Green
+    $d2 - $d1 | Select-Object Minutes, Seconds | Out-Host
+    
+    $startDateTime = $StartDateTime.AddDays(1)
+    #{$name = Get-Date(Get-Date).AddDays($i) -f "dd.MM.yyyy" }
+  }
+
+
+
+ 
+
+
+ 
+
+
+
+<# Константин функция
+# Итоговая строка подключения
+ $connectionString = "Data Source=$DataSource; Initial Catalog=$database; $auth"
+
+
+$Result = SqlConnect -ConnectionString $connectionString -Query $Query -DBTypeConnection MSSql -Operation Read
+$Result = SqlConnect -ConnectionString $connectionString -Query $Query -Variable $Variable -DBTypeConnection MSSql -Operation Read  # C переменными не удалось заставить работать
+#$Result | Export-Csv -Path "$PathScript\$NameScript\SR_BPM7_$startDateFormat.csv" -Encoding UTF8 -NoTypeInformation
+ $result | Export-Csv -Path "D:\Scripts\PowerShell\SQL\TFS_370389_v2.0\SR_BPM7_$($anme).csv" -Encoding UTF8 -NoTypeInformation
+
+function SQLConnect (
+    [string]$ConnectionString,
+    [string]$Query,
+    #[string]$Variable,   # C переменными не удалось заставить работать
+    [ValidateSet("MSSql", "Oracle")][String]$DBTypeConnection,
+    [ValidateSet("Read", "Write")][String]$Operation
+) {
+    switch ($DBTypeConnection) {
+        "MSSql" {
+            $con = new-object "System.data.sqlclient.SQLconnection"
+            $con.ConnectionString =$ConnectionString
+            $con.open()
+            $sqlcmd = new-object "System.data.sqlclient.sqlcommand"
+            $sqlcmd.connection = $con
+            $sqlcmd.CommandTimeout = 10000000
+            $sqlcmd.CommandText = $Query
+            #$sqlCmd.Parameters.Add('@StartDate',$Variable) # C переменными не удалось заставить работать
+            switch ($Operation) {
+                "Read"  {
+                    $result = $sqlcmd.ExecuteReader()
+                    $Table = [System.Data.DataTable]::new()
+                    $Table.Load($result)
+                    $con.Close()
+                    $Table.Columns.ColumnName | % { $Table.Columns[$_].ColumnName = $_.ToUpper()}
+                    return ,$Table
+                }
+                "Write" {
+                    [void]::($sqlcmd.ExecuteNonQuery())
+                    $con.Close()
+                }
+            }
+        }
+        "Oracle" {
+            $con = New-Object Oracle.ManagedDataAccess.Client.OracleConnection($ConnectionString)
+            $con.Open()
+            $cmd = $con.CreateCommand()
+            $cmd.CommandText = $Query
+            switch ($Operation) {
+                "Read" {
+                    $result = $cmd.ExecuteReader()
+                    $Table = New-Object "System.Data.DataTable"
+                    $Table.Load($result)
+                    $Table.Columns.ColumnName | % { $Table.Columns[$_].ColumnName = $_.ToUpper()}
+                }
+                "Write" {
+                    $cmd.Transaction = $con.BeginTransaction()
+                    [void]::($cmd.ExecuteNonQuery())
+                    $cmd.Transaction.Commit()
+                }
+            }
+            $con.Close()
+            switch ($Operation) {
+                "Read" {return ,$Table}
+                "Write" {return}
+            }
+        }
+    }
+}
+
+
+# Тут что-то из высшего пилотажа - не смог осилить
+    $query = @"
+        select top 10 * from [case] where number in (
+    "@
+
+    @("sr01","SR02") | % {
+        $query += "@"
+            '$_',
+    "@
+    }
+    $query = $query.Substring(0.$Query.Length-1)
+    $query += ")"  #>
+
+
+#>
+
+
+
+
+
+
+<#
+###   sqlcmd   ###
+
+# Get-Command -Module SQLPS
 # Install-Module -Name SqlServer -RequiredVersion 21.1.18256
 # cd D:\Programms\DataBase
 # Install-Script -Name sqlserver.21.1.18256.nupkg
 
 
-# Create folder for csv file
-#($myInvocation.MyCommand.Name).Basename 
-$NameScript = (Get-Item $myInvocation.myCommand.path).Basename
-$PathScript = (Get-Item $myInvocation.myCommand.path).DirectoryName
-$PathScript
-#[System.IO.Path]::GetFileNameWithoutExtension("$myInvocation.myCommand.path")
-#[System.IO.Path]::GetFileNameWithoutExtension($myInvocation.MyCommand.Name)
-New-Item -Path . -Name $NameScript -ItemType "Directory"
-[Environment]::NewLine
-
-#[string]$FileSQL = [System.IO.Path]::Combine(([System.IO.Path]::GetDirectoryName($MyInvocation.InvocationName)), ([System.IO.Path]::ChangeExtension($MyInvocation.InvocationName, "sql" )))
-[string]$FileSQL = [System.IO.Path]::Combine(([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Name)), ([System.IO.Path]::ChangeExtension($MyInvocation.MyCommand.Name, "sql" )))
-
-
-# Server, base, table, auth
-#$dataSource = “server\instance”
- $dataSource = Read-Host "Адрес сервера"
-
-#$database = “master”
-$database = Read-Host "Имя базы данных"
-
-$auth = “Integrated Security=SSPI;”
-#$auth = Get-Credential
-
-
-###   sqlcmd   ###
-[Environment]::NewLine
-#$StringArray = "MYVAR1='String1'", "MYVAR2='String2'"
-[DateTime]$StartDateTime = Read-Host "Start Date ( yyyy-MM-dd ) Example: 2022-01-01 "
-[DateTime]$EndDateTime   = Read-Host "  End Date ( yyyy-MM-dd ) Example: 2022-05-31 "
-
-
 while ($StartDateTime -le $EndDateTime) {
 
   [Environment]::NewLine
- #$StartDate000000 = $StartDate+" 00:00:00"
- #$StartDate235959 = $StartDate+" 23:59:59"
-  
  #$StartDateTime.ToString("yyyy-MM-dd")     
  #$StartDate = $StartDateTime.ToString("yyyy-MM-dd")  # не работает
   $StartDate = '{0:yyyy-MM-dd}' -f $StartDateTime
+  $StartDateFormat = '{0:ddMMyyy}' -f $StartDateTime
   $StartDate
 
   
    $InvokeVariables = @("StartDate = '$StartDate'")
   #$InvokeVariables = @("StartDate000000 = '$StartDate000000'", "StartDate235959 = '$StartDate235959'")
-
+  #$StringArray = "MYVAR1='String1'", "MYVAR2='String2'"
 
    $d1 = Get-Date  # Фиксируем время начала отработки скрипта
-   Invoke-sqlcmd -ServerInstance $dataSource -Database $database -InputFile $FileSQL -Variable $InvokeVariables | Export-Csv -Path "$PathScript\$NameScript\$startDate.csv" -Encoding UTF8
-   #-Credential -OutputAs
+   Invoke-sqlcmd -ServerInstance $dataSource -Database $database -InputFile $FileSQL -Variable $InvokeVariables `
+    | Export-Csv -Path "$PathScript\$NameScript\SR_BPM7_$startDateFormat.csv" -Encoding UTF8 -Delimiter "^" -NoTypeInformation
+   #| Select-Object -Property "Перечень параметров" `
+    
+   #-Credential -OutputAs    # 
    $d2 = Get-Date  # Фиксируем время окончания отработки скрипта
    'Время выполнения:'
    $d2 - $d1 | Select-Object Minutes, Seconds | Out-Host
@@ -64,7 +267,7 @@ while ($StartDateTime -le $EndDateTime) {
 
   $startDateTime = $StartDateTime.AddDays(1)
 }
-
+#>
 
 
 
@@ -97,33 +300,7 @@ $dataset.Tables[0] | Export-Csv -Path "$PathScript\$NameScript\$NameScript.csv" 
 #>
 
 
-###   SslConnection   ###
-<#$SqlServer = "server\instance";
- $SqlServer = "t2ru-tr-tst-02.corp.tele2.ru";
-#$SqlCatalog = "база_данных";
- $SqlCatalog = "BPMonline_80";
 
- $SqlLogin = "";
-#$SqlLogin = Read-Host "пользователь (через t2ru\)"
- $SqlPassw = ""
-#$SqlPassw = Read-Host "пароль" -AsSecureString
-#$auth = Get-Credential
-
-$SqlConnection = New-Object System.Data.SqlClient.SqlConnection
-#$SqlConnection.ConnectionString = "Server=$SqlServer; Database=$SqlCatalog; Integrated Security=True"
- $SqlConnection.ConnectionString = "Server=$SqlServer; Database=$SqlCatalog; User ID=$SqlLogin; Password=$SqlPassw;"
-#$SqlConnection.ConnectionString = "Server=$SqlServer; Database=$SqlCatalog; $auth;"
-
-$SqlConnection.Open()
-$SqlCmd = $SqlConnection.CreateCommand()
-$SqlCmd.CommandText = "SELECT TOP (10) FROM dbo.account"
-$objReader = $SqlCmd.ExecuteReader()
-while ($objReader.read()) {
-  echo $objReader.GetValue(0)
-}
-$objReader.close()
-$SqlConnection.close()
-#>
 
 
 
